764de42c01f654cba1a34085ced66fd60eae23ae
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2025-12-30 16:09:48 -0300

    feat: Seller Model (closes #1)

---
+++ backend/src/main/java/com/sellers/backend/models/Seller.java
@@ -0,0 +1,85 @@
+package com.sellers.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_SELLER")
+public class Seller {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 255)
+  private String name;
+
+  @Column(nullable = false)
+  private Double salary;
+
+  private Double bonus;
+
+  @Column(nullable = false)
+  private Integer gender;
+
+  public Seller() {
+  }
+
+  public Seller(String name, Double salary, Double bonus, Integer gender) {
+    this.name = name;
+    this.salary = salary;
+    this.bonus = bonus;
+    this.gender = gender;
+  }
+
+  public Seller(Long id, String name, Double salary, Double bonus, Integer gender) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+    this.bonus = bonus;
+    this.gender = gender;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(Double salary) {
+    this.salary = salary;
+  }
+
+  public Double getBonus() {
+    return bonus;
+  }
+
+  public void setBonus(Double bonus) {
+    this.bonus = bonus;
+  }
+
+  public Integer getGender() {
+    return gender;
+  }
+
+  public void setGender(Integer gender) {
+    this.gender = gender;
+  }
+}
