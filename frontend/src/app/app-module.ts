17ca1eb1676519d4cf7f64cf337f2b9bd83eb1a0
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2025-12-31 17:09:06 -0300

    chore: create Maven and Angular Projects (closes #9)

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -1,8 +1,16 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { LOCALE_ID, NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import localePt from '@angular/common/locales/pt';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { registerLocaleData } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -10,10 +18,17 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    },
+    provideHttpClient()
   ],
   bootstrap: [App]
 })
